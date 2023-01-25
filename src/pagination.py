219bb56702963fe3eabcb2a82bd841e7ff1cf456
Nahum Sá <nahumsaa@gmail.com>
2023-01-24 23:12:36 -0300

    move pagination error handling primer to pagination.py

---
+++ src/pagination.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+
+class ExecutionError(Exception):
+    pass
+
+page_number = 1
+max_pages = 10
+try:
+    if driver.title == "502 Bad Gateway":
+        raise ExecutionError(f"scrapper received a 502 status code when performing a pagination at page {page_number}")
+
+except ExecutionError as e:
+    error_json = {
+        "error": {
+        "occured_at": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "status_code": 502,
+        "message": f"scrapper received a 502 status code when performing a pagination at page {page_number}",
+        "scrapper_state": {
+           "current_page": page_number,
+            "total_of_pages": max_pages,
+            "search_params": {
+                "judgment_date": {
+                    "start_at": datetime.strptime(search_start_date, "%d/%m/%Y").strftime("%Y%m%d"),
+                    "end_at": datetime.strptime(search_end_date, "%d/%m/%Y").strftime("%Y%m%d")
+                    }
+                }
+            }
+        }
+    }
+
+    raise e
--- src/scrape.py
+++ src/scrape.py
@@ -49,7 +49,6 @@ def get_decision_element(number_in_list: int):
 decision = get_decision_element(1)
 decision.click()
 
-
 change_to_window_handle(driver, 1)
 
 decision_detail_repository = DecisionDetailsPageRepository(session=driver)
@@ -63,34 +62,3 @@ driver.close()
 change_to_window_handle(driver, 0)
 
 print(data)
-
-
-from datetime import datetime
-class ExecutionError(Exception):
-    pass
-
-page_number = 1
-max_pages = 10
-try:
-    if driver.title == "502 Bad Gateway":
-        raise ExecutionError(f"scrapper received a 502 status code when performing a pagination at page {page_number}")
-except ExecutionError as e:
-    error_json = {
-        "error": {
-        "occured_at": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
-        "status_code": 502,
-        "message": f"scrapper received a 502 status code when performing a pagination at page {page_number}",
-        "scrapper_state": {
-           "current_page": page_number,
-            "total_of_pages": max_pages,
-            "search_params": {
-                "judgment_date": {
-                    "start_at": datetime.strptime(search_start_date, "%d/%m/%Y").strftime("%Y%m%d"),
-                    "end_at": datetime.strptime(search_end_date, "%d/%m/%Y").strftime("%Y%m%d")
-                    }
-                }
-            }
-        }
-    }
-
-    raise e
